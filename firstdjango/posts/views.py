00ed6645d82c84a226e96f93ad69c99188897f14
dinasalina <syazwan.adnan@treasury.gov.my>
2025-06-16 14:36:16 +0800

    setup home page

--- firstdjango/posts/views.py
+++ firstdjango/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def home(request):
+    post_list = Post.objects.all()
+
+    context = {
+        'post_list': post_list,
+    }
+
+    return render(request, "home.html", context)
+    
+    
