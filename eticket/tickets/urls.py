38c19e497044205f0807be9ae17d03efa0b40635
dinasalina <syazwan.adnan@treasury.gov.my>
2025-06-17 15:42:11 +0800

    intial cretae ticketn form

--- eticket/tickets/urls.py
+++ eticket/tickets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import  path
 
-from .views import home
+from .views import home, create
 
 urlpatterns = [
 
      path('', home, name='home'),
+     path('tickets/create/', create, name='create_ticket'),
    
 ]
--- eticket/tickets/views.py
+++ eticket/tickets/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from .models import Tickets
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def create(request):
+
+    if request.method == "POST":
+        # bila sumbit form
+        print("Form submitted")
+        print("From data", request.POST)
+
+        # get data from request
+        title = request.POST.get('title', '')
+        description = request.POST.get('description', '')
+
+        user = request.user
+        
+        # try crete ticket
+        Tickets.objects.create(title=title, description=description, user=user)
+
+    else:
+        # bila load form
+        pass
+
+    return render(request, "tickets/create.html")
